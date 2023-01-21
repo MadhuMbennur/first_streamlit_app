d2b37ceb0706e26e905cdd9bc4be2d831eb59147
MadhuMbennur <123229462+MadhuMbennur@users.noreply.github.com>
2023-01-21 12:33:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('hi madhu')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
